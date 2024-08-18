8fa85dd0ae7af60ff2bbeefc94a9982c39391bde
Hosna Jabarooti <hosnajabarooti@gmailk.com>
2024-08-18 18:55:25 +0330

    config sample commented

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+
+// module.exports = {
+//   darkMode: 'class', // or 'media' for OS-based dark mode
+//   theme: {
+//     extend: {
+//       colors: {
+//         light: {
+//           background: '#ffffff',
+//           text: '#333333',
+//           primary: '#007BFF',
+//           secondary: '#6C757D',
+//         },
+//         dark: {
+//           background: '#1a202c',
+//           text: '#e2e8f0',
+//           primary: '#3182ce',
+//           secondary: '#718096',
+//         },
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
+
+/* <div class="bg-light-background text-light-text dark:bg-dark-background dark:text-dark-text">
+  <h1 class="text-light-primary dark:text-dark-primary">Hello, World!</h1>
+  <p class="text-light-secondary dark:text-dark-secondary">This is a paragraph.</p>
+</div> */
+//  #EEF5FF 
+// #F8FBFF
+// #F9FBFF
+// #F8FCFF
