6e5d558200530514a58235cb5434d38fd5089d5d
Hosna Jabarooti <hosnajabarooti@gmailk.com>
2024-08-24 00:32:25 +0330

    some extensions added

--- myConfigs.js
+++ myConfigs.js
@@ -1,9 +1,14 @@
 tailwind.config = {
+  darkMode: 'class',
     theme: {
       extend: {
         fontSize: {
           'custom-sm': '10px',
           'custom-4xl': '40px',
+          'custom-14': '14px',
+          'custom-32': '32px',
+          'custom-28': '28px',
+          'custom-50': '50px',
           
         },
         spacing: {
@@ -13,6 +18,9 @@ tailwind.config = {
         lineHeight: {
             'custom-72': '72px',
             'custom-30': '30px'
+        },
+        margin: {
+          'custom-ten': '10px'
         },
         height :{
           // '266px': '266px'
