67ef609314cf1369b124c174045d8c102e0f1ccb
Hosna Jabarooti <hosnajabarooti@gmailk.com>
2024-08-23 19:55:26 +0330

    some extensions added in new config

---
+++ myConfigs.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        fontSize: {
+          'custom-sm': '10px',
+          'custom-4xl': '40px',
+          
+        },
+        spacing: {
+          'custom-ten': '10px',
+          
+        },
+        lineHeight: {
+            'custom-72': '72px',
+            'custom-30': '30px'
+        },
+        height :{
+          // '266px': '266px'
+        //   'custom-72': '72px'
+        },
+      },
+    },
+    plugins: [],
+  };
