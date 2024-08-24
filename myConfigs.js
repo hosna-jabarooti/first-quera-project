236539cda398167d949585ba94e1a89923688c24
Hosna Jabarooti <hosnajabarooti@gmailk.com>
2024-08-25 00:29:09 +0330

    typography updated

--- myConfigs.js
+++ myConfigs.js
@@ -6,22 +6,28 @@ tailwind.config = {
         'custom-sm': '10px',
         'custom-4xl': '40px',
         'custom-14': '14px',
+        'custom-15': '15px',
         'custom-28': '28px',
         'custom-32': '32px',
+        'custom-46': '46px',
         'custom-28': '28px',
         'custom-50': '50px',
+        'custom-64': '64px',
       },
       spacing: {
         'custom-ten': '10px',
         'custom-60': '60px',
         'custom-330': '330px',
         'custom-156': '156px',
+        'custom-350': '350px',
 
       },
       lineHeight: {
+        'custom-18': '18px',
         'custom-72': '72px',
         'custom-30': '30px',
-        'custom-82': '82px',
+        'custom-50': '50px',
+        'custom-80': '80px',
         'custom-115': '115px'
       },
       margin: {
