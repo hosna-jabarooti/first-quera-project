b0742dccd7e772cd52904bd39af990a951f11622
Hosna Jabarooti <hosnajabarooti@gmailk.com>
2024-08-25 15:07:35 +0330

    cli tailwind added!!!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,67 @@
+module.exports = {
+content : ["./index.html", "./about-us.html", "./contact-us.html"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontSize: {
+        'custom-sm': '10px',
+        'custom-4xl': '40px',
+        'custom-14': '14px',
+        'custom-15': '15px',
+        'custom-28': '28px',
+        'custom-32': '32px',
+        'custom-46': '46px',
+        'custom-28': '28px',
+        'custom-50': '50px',
+        'custom-64': '64px',
+      },
+      spacing: {
+        'custom-ten': '10px',
+        'custom-60': '60px',
+        'custom-330': '330px',
+        'custom-156': '156px',
+        'custom-350': '350px',
 
-// module.exports = {
-//   darkMode: 'class', // or 'media' for OS-based dark mode
-//   theme: {
-//     extend: {
-//       colors: {
-//         light: {
-//           background: '#ffffff',
-//           text: '#333333',
-//           primary: '#007BFF',
-//           secondary: '#6C757D',
-//         },
-//         dark: {
-//           background: '#1a202c',
-//           text: '#e2e8f0',
-//           primary: '#3182ce',
-//           secondary: '#718096',
-//         },
-//       },
-//     },
-//   },
-//   plugins: [],
-// }
+      },
+      lineHeight: {
+        'custom-18': '18px',
+        'custom-72': '72px',
+        'custom-30': '30px',
+        'custom-50': '50px',
+        'custom-80': '80px',
+        'custom-115': '115px'
+      },
+      margin: {
+        'custom-ten': '10px'
+      },
+      height: {
+        // '266px': '266px'
+        'custom-cir': '450px'
+      },
+      width: {
+        'custom-cir': '450px'
+      },
+      colors: {
+        dark: {
+          primary: "#3A86F8",
+          "primary-hover-dark": "#6BA4FA",
+          "background-dark": "#040810",
+          "on-background-dark": "#E6E9EF",
+          "surface-dark": "#E6E9EF",
+          "a-dark": "#CFCFCF",
+          "a-hover-dark": "#037FFF",
+          "text-color": "#ffffff",
+          "about-bg": "#122549",
+          "card-bg": "#0B192D",
+          "about-me-card": "#0E1E34",
+          "white-text": "#E6E6E6",
+          "custom-gray": "#B5B5B5",
+          "light-gray": "#ACACAC",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
 
-/* <div class="bg-light-background text-light-text dark:bg-dark-background dark:text-dark-text">
-  <h1 class="text-light-primary dark:text-dark-primary">Hello, World!</h1>
-  <p class="text-light-secondary dark:text-dark-secondary">This is a paragraph.</p>
-</div> */
-//  #EEF5FF 
-// #F8FBFF
-// #F9FBFF
-// #F8FCFF
+
